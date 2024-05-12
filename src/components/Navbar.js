53edffa249d5463ddfa70375905a6f4e9a1cb77d
DohunHyun <dohun.hyun@gmail.com>
2024-05-12 22:41:22 +0900

    feat : 상단 네비게이션 바 1차 완성

---
+++ src/components/Navbar.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import styles from '../styles/Navbar.module.css';
+import backButton from '../image/backBtn.svg';
+import profileImg from '../image/profile.jpeg';
+
+function Navbar() {
+    const navigate = useNavigate();
+    const [ showDropdown, setShowDropdown ] = useState(false);
+
+    const toggleDropdown = () => setShowDropdown(!showDropdown);
+
+    return (
+        <div className={styles.navbar}>
+            <div className={styles.navContent}>
+                <div onClick={() => navigate(-1)} className={styles.backButton}>
+                    <img src={backButton} className={styles.backBtnImage} />
+                </div>
+                <div className={styles.titleText}>
+                    아무 말 대잔치
+                </div>
+                <div className={styles.profileBtn}>
+                    <img onClick={toggleDropdown} src={profileImg} className={styles.profileImage}></img>
+                    {showDropdown && (
+                        <div className={styles.dropDown}>
+                            <button onClick={() => console.log('회원정보 수정')} className={styles.dropDownBox}>회원정보 수정</button>
+                            <button onClick={() => console.log('비밀번호 수정')} className={styles.dropDownBox}>비밀번호 수정</button>
+                            <button onClick={() => console.log('로그아웃')} className={styles.dropDownBox}>로그아웃</button>
+                        </div>
+                    )}
+                </div>
+            </div>
+            <hr className={styles.hr}/>
+        </div>
+    );
+}
+
+export default Navbar;
