22b4c290d02da2f9a3e6b7f085e7ebfd1ea8446a
DohunHyun <dohun.hyun@gmail.com>
2024-05-13 14:19:50 +0900

    feat : 회원가입 화면 1차 구현

--- src/pages/LogInPage.js
+++ src/pages/LogInPage.js
@@ -2,20 +2,20 @@ import styles from '../styles/LogIn.module.css';
 
 function LogInPage() {
     return (
-        <form id="form" class={styles.body}>
-            <div class={styles.input}>
-                <p class={styles.title}>로그인</p>
+        <form id="form" className={styles.body}>
+            <div className={styles.input}>
+                <p className={styles.title}>로그인</p>
                 <div>
-                    <p class={styles.text}>이메일</p>
+                    <p className={styles.text}>이메일</p>
                     <input type="email" placeholder="이메일을 입력하세요" required />
-                    <p class={styles.text}>비밀번호</p>
+                    <p className={styles.text}>비밀번호</p>
                     <input type="password" placeholder="비밀번호를 입력하세요" required />
-                    <p class={styles.helper}></p>
+                    <p className={styles.helper}></p>
                 </div>
             </div>
-            <div class={styles.button}>
-                <button id="login-btn" type="submit" class={`${styles.loginBtn} ${styles.btnInactive}`}>로그인</button><br />
-                <a class={styles.signIn} href="/signin" display="block">회원가입</a>
+            <div className={styles.button}>
+                <button id="login-btn" type="submit" className={`${styles.loginBtn} ${styles.btnInactive}`}>로그인</button><br />
+                <a className={styles.signIn} href="/signin" display="block">회원가입</a>
             </div>
         </form>
     );
--- src/pages/SignInPage.js
+++ src/pages/SignInPage.js
@@ -1,6 +1,41 @@
+import styles from '../styles/SignIn.module.css';
+
 function SignInPage() {
     return (
-        <>회원가입 페이지</>
+        <>
+            <form className={styles.body} onsubmit="return checkForm()">
+                <div className={styles.input}>
+                    <p className={styles.title}>회원가입</p>
+                    <div>
+                        <p className={styles.text}>프로필 사진</p>
+                        <p id="profile-helper" className={styles.helper}>* 프로필 사진을 추가해주세요.</p>
+                        <div className={styles.center}>
+                            <div className={styles.profileDiv}>
+                                <div className={styles.profileDefault}>+</div>
+                                <img className={styles.profileImg} />
+                            </div>
+                            <input id="profile-img-btn" type="file" accept="image/*" className={styles.displayNone}/>
+                        </div>
+                        <p className={styles.text}>이메일*</p>
+                        <input id="email-input" type="email" placeholder="이메일을 입력하세요" />
+                        <p id="email-helper" className={styles.helper}>* 이메일을 입력해주세요.</p>
+                        <p className={styles.text}>비밀번호*</p>
+                        <input id="password-input" type="password" placeholder="비밀번호를 입력하세요" />
+                        <p id="password-helper" className={styles.helper}>* 비밀번호를 입력해주세요.</p>
+                        <p className={styles.text}>비밀번호 확인*</p>
+                        <input id="passwordCheck-input" type="password" placeholder="비밀번호를 입력하세요" />
+                        <p id="passwordCheck-helper" className={styles.helper}>* 비밀번호를 한번 더 입력해주세요.</p>
+                        <p className={styles.text}>닉네임*</p>
+                        <input id="nickname-input" type="text" placeholder="닉네임을 입력하세요" />
+                        <p id="nickname-helper" className={styles.helper}>* 닉네임을 입력해주세요.</p>
+                    </div>
+                </div>
+                <div className={styles.button}>
+                    <button id="signin-btn" className={`${styles.signinBtn} ${styles.btnInactive}`}>회원가입</button><br />
+                    <a className={styles.logIn} href="/login">로그인하러 가기</a>
+                </div>
+            </form>
+        </>
     );
 }
 
